d5968f972ad17dd94527945bcd195730d5d39b34
Kevin Kao <kk415kk@gmail.com>
2014-12-22 16:05:17 -0800

    Added contact functionality, validation checks

--- api/controllers/HomeController.js
+++ api/controllers/HomeController.js
@@ -8,6 +8,41 @@
 module.exports = {
 	index: function(req, res) {
     res.view({ title: 'Home' });
+  },
+
+  contact: function(req, res) {
+    var nodemailer = require('nodemailer');
+    var transporter = nodemailer.createTransport(sails.config.transporter);
+    var mailOptions = sails.config.mailOptions;
+
+    var params = req.params.all();
+
+    var firstName = params['firstName'],
+        lastName = params['lastName'],
+        type = params['type'],
+        emailAddress = params['emailAddress'],
+        emailMessage = params['emailMessage']
+
+    var HTMLtext = ''
+    HTMLtext += '<b>From: </b>' + firstName + ' ' + lastName;
+    HTMLtext += '<br><b>Type: </b>' + type;
+    HTMLtext += '<br><b>Email: </b>' + emailAddress;
+    HTMLtext += '<br><b>Message: </b><br>';
+    HTMLtext += emailMessage;
+
+    mailOptions['subject'] = 'Message from kevkao.com'
+    mailOptions['text'] = HTMLtext
+    mailOptions['html'] = HTMLtext
+
+    transporter.sendMail(mailOptions, function(error, info) {
+      if (error) {
+        sails.log(error)
+        return res.json({ success: false });
+      } else {
+        sails.log('Message sent: ' +  info.response);
+        return res.json({ success: true });
+      }
+    });
   }
 };
 
